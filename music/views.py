e8b228216d373917c26e6790b1ad5fa20209a456
Pritish Mishra <pritishjan@gmail.com>
2020-08-23 13:00:58 +0530

    another commti

--- music/views.py
+++ music/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from musicbeats.models import Song
+from musicbeats.models import Watchlater
+from django.db.models import Case, When
 
 def index(request):
     song = Song.objects.all()[0:3]
-    return render(request, 'index.htm', {'song': song})
+    wl = Watchlater.objects.filter(user=request.user)
+    ids = []
+    for i in wl:
+        ids.append(i.video_id)
+    
+    preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(ids)])
+    watch = Song.objects.filter(song_id__in=ids).order_by(preserved) 
+    watch = reversed(watch)
+    return render(request, 'index.htm', {'song': song, 'watch': watch})
